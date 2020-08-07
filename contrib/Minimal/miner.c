6f0b3cc089705331bf345a62811b7f6c56547dc9
potatowipedlifereverse <potatolivingstonei2p@gmail.com>
2020-08-07 18:49:44 +0300

    delete

--- contrib/Minimal/miner.c
+++ contrib/Minimal/miner.c
@@ -14,20 +14,3 @@ struct addrBox miner(const char* needle){
 	return ret;		
 		
 }
-
-/*
-Example for debug:
-	puts("PublicKey");
-	for(int i =0; i < sizeof(keys.PublicKey);i++){
-		printf( "%02x", keys.PublicKey[i]);
-	}
-	puts("\nPrivateKey");
-	for(int i =0; i < sizeof(keys.PrivateKey);i++){
-		printf( "%02x", keys.PrivateKey[i]);
-	}
-	puts("");
-
-	//printf("IPv6: %s\r\nRawPublicKey: %s\r\nRawPrivateKey: %s\r\n", ipv6, keys.PublicKey, keys.PrivateKey);
-	//printf("IPv6: %s\r\n", ipv6);
-	//sleep(5);
-*/
