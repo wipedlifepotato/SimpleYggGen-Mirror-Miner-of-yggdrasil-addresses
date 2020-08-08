616f64546892095529ba931b64d111026e8f4c6f
potatowipedlifereverse <potatolivingstonei2p@gmail.com>
2020-08-08 07:34:20 +0300

    winsupp

--- contrib/Minimal/ipv6.h
+++ contrib/Minimal/ipv6.h
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #else
 #include <winsock2.h>
+#define _WIN32_WINNT _WIN32_WINNT_WIN8 // Windows 8.0
+#include <ws2tcpip.h> // -lws2_32
 #endif
 #include <unistd.h>
 char *convertSHA512ToIPv6(unsigned char hash[SHA512_DIGEST_LENGTH]) ;
